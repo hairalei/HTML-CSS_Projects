8dc0a701c56c87983bda65f2dae127491dfb4548
hairalei <darhaii13@gmail.com>
2022-05-05 16:52:15 +0800

    Add event listeners for open and close nav

--- script.js
+++ script.js
@@ -3,3 +3,23 @@
 ////Footer
 const year = document.querySelector(".year");
 year.textContent = new Date().getFullYear();
+
+/////NAV
+const headerNav = document.querySelector(".header");
+
+function openNav() {
+  headerNav.classList.toggle("nav-open");
+}
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener("click", openNav);
+
+////close nav when link is clicked
+
+headerNav.addEventListener("click", function (e) {
+  //   console.log(e.target.classList);
+  if (!e.target.classList.contains("nav_link")) return;
+
+  headerNav.classList.remove("nav-open");
+});
